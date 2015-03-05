2006df639d9c3e76addd7ba99abcb1c2ddaa01cc
Yann Prono <pronoyann@gmail.com>
2015-03-05 20:32:37 +0100

    Add comments :)

--- app/controllers/commentaires_controller.rb
+++ app/controllers/commentaires_controller.rb
@@ -1,8 +1,67 @@
 class CommentairesController < ApplicationController
+  before_action :set_commentaire, only: [:show, :edit, :update, :destroy]
+
+  # GET /commentaires
+  # GET /commentaires.json
+  def index
+    @commentaires = Commentaire.all
+  end
+
+  # GET /commentaires/1
+  # GET /commentaires/1.json
   def show
   end
 
-  def create
-  	 @commentaire = Commentaire.new
+  # GET /commentaires/new
+  def new
+    @commentaire = Commentaire.new
   end
+
+  # GET /commentaires/1/edit
+  def edit
+  end
+
+  # POST /commentaires
+  # POST /commentaires.json
+  def create
+  	@item = Item.find(params[:item_id])
+    @commentaire = @item.commentaires.create(commentaire_params)
+    @commentaire = params[:item_id]
+    redirect_to @item
+  end
+
+  # PATCH/PUT /commentaires/1
+  # PATCH/PUT /commentaires/1.json
+  def update
+    respond_to do |format|
+      if @commentaire.update(commentaire_params)
+        format.html { redirect_to @commentaire, notice: 'Commentaire was successfully updated.' }
+        format.json { render :show, status: :ok, location: @commentaire }
+      else
+        format.html { render :edit }
+        format.json { render json: @commentaire.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /commentaires/1
+  # DELETE /commentaires/1.json
+  def destroy
+    @commentaire.destroy
+    respond_to do |format|
+      format.html { redirect_to commentaires_url, notice: 'Commentaire was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_commentaire
+      @commentaire = Commentaire.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def commentaire_params
+      params.require(:commentaire).permit(:email, :contenu, :item_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :commentaires
 
   resources :sessions
   get 'login' => 'sessions#new', :as => 'login'
@@ -10,6 +9,8 @@ Rails.application.routes.draw do
   get "signup" => "users#new", :as => "sign_up"
   resources :users
 
+  
+
 
   get 'items/couleur/:color' => 'items#filter_by_color'
   get 'items/type/:id' => 'items#filter_by_type'
@@ -17,7 +18,9 @@ Rails.application.routes.draw do
 
   resources :types
   resources :pieces
-  resources :items
+  resources :items do
+    resources :commentaires, :only => [:create]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
