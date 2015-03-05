337781671b164449e66827c6123c38a83b481c60
Ysee MONNIER <yseemonnier@gmail.com>
2015-03-05 22:01:43 +0100

    add button sign-user + fr -> en

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,38 +1,39 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     //Tell Rails that we’re sending a JavaScript request
     /*$.ajaxSetup({ 
-        'beforeSend': function (xhr){
-        xhr.setRequestHeader("Accept", "text/javascript")}  
-    });*/
-    $(".from_piece").change(function() {
+     'beforeSend': function (xhr){
+     xhr.setRequestHeader("Accept", "text/javascript")}
+     });*/
+    $(".from_piece").change(function () {
         $.ajax({
             url: '/items/from_piece',
             dataType: 'text',
             data: {
                 id: $(this).val()
             },
-            success : function(result) {
+            success: function (result) {
                 eval(result);
             },
-            error : function(xhr, ajaxOptions, thrownError) {
+            error: function (xhr, ajaxOptions, thrownError) {
                 alert(thrownError);
             }
         });
     });
 
-    $('.from_type').change(function() {
+    $('.from_type').change(function () {
         $.ajax({
             url: '/items/from_type',
             dataType: 'text',
             data: {
                 id: $(this).val()
             },
-            success : function(result) {
+            success: function (result) {
                 eval(result);
             },
-            error : function(xhr, ajaxOptions, thrownError) {
+            error: function (xhr, ajaxOptions, thrownError) {
                 alert(thrownError);
             }
         });
     });
 });
+
